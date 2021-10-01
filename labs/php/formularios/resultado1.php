e44ac3712ab36d48e23c1691c903dd993daf2f89
Maicon <maicon.brassa@gmail.com>
2021-10-01 02:03:45 -0300

    atualizando

---
+++ labs/php/formularios/resultado1.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Maicon Brassanini">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Formulários em PHP</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/checkout/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+    
+<div class="container">
+  <main>
+    <div class="py-5 text-center">
+      <img class="d-block mx-auto mb-4" src="../assets/brand/mb_logo.png" alt="" width="120" height="85">
+      <h2>Formulários</h2>
+      
+    </div>
+
+    
+      <div class="col-md-6 col-lg-8">
+        <h4 class="mb-3">Exercício 02 PHP</h4>
+        <?php 
+            $nota1 = $_GET["nota1"];
+            $nota2 = $_GET["nota2"];
+            $media = ($nota1 + $nota2)/2;
+                if ($media >= 7){
+                echo "<p>A média entre $nota1 e $nota2 é igual a $media </p>
+                <br>
+                Situação do aluno: <h3 style='color:red;'><b>APROVADO</b></h3>";
+                } else{
+                echo "<p>A média entre $nota1 e $nota2 é igual a $media </p>
+                <br>
+                Situação do aluno: <h3 style='color:red;'><b>REPROVADO</b></h3>";
+                }
+            
+
+           
+
+        ?>
+
+       <h4><a href="media.html">Voltar</a></h4>
+    
+
+      <div class="col-sm-6">
+             
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    
+  </footer>
+</div>
+
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js></script>
+
+      <script src="form-validation.js"></script>
+  </body>
+</html>
+
