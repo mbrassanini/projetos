af0248cd40d1122180964038e1fa6c96c59e99d7
Maicon <maicon.brassa@gmail.com>
2021-11-02 03:37:23 -0300

    novas pastas e arquivos React

---
+++ labs/javascript/teste/main.js
@@ -0,0 +1,14 @@
+var num1;
+var num2;
+var soma;
+
+alert("Digite numero 1: " + gets(num1));
+alert("Digite numero 2: " + gets(num2));
+
+function calcSoma(){
+    soma = num1 + num2;
+    console.log("A média foi: " + soma);
+}
+
+
+calcSoma();
